2fe12b6601a2d200125c58d0ebcf80ce7ea91815
yuuun <ynkim31@naver.com>
2021-08-16 16:24:25 +0900

    TBD: model fix

--- main.py
+++ main.py
@@ -24,30 +24,31 @@ if __name__=='__main__':
         
     model = GCN(nfeat = data.features.shape[1], nhid=160, dropout=0.5)
     optimizer = optim.Adam(model.parameters(), lr=0.0001)
+    adj = data.fadj
+    norm = adj.shape[0] * adj.shape[0] / float((adj.shape[0] * adj.shape[0] - adj.sum()) * 2)
 
     
     for epoch in range(0, 10001):
         start = time.time()
         model.train()
         optimizer.zero_grad()
-        output = model(data.features, data.nfadj)
-        loss_train = F.nll_loss(output[data.idx_train], lab[data.idx_train])
+        adj_pred = model(data.features, data.fadj)
+
+        loss = norm * F.binary_corss_entropy(adj_pred.view(-1), adj.to_dense().view(-1))
+        #loss_train = F.nll_loss(output[data.idx_train], lab[data.idx_train])
             
-        loss_train.backward()
+        loss.backward()
         optimizer.step()
         if epoch % 1000 == 0:
             torch.save({
                 'epoch': epoch,
                 'model_state_dict': model.state_dict(),
                 'optimzier_state_dict': optimizer.state_dict(),
-                'loss': loss_train,
+                'loss': loss,
                 }, './pt/test.pt')
-            print('{:2d} {:.4f} {:.2f}s\n'.format(epoch, loss_train.item(), time.time() - start), end='\t')
+            print('{:2d} {:.4f} {:.2f}s\n'.format(epoch, loss.item(), time.time() - start), end='\t')
 
             with torch.no_grad():
                 model.eval()
                 output = model(data.features, data.nfadj)
-                
-                outputs = nn.Sigmoid()(output) > 0.5  # uses sigmoid function in test case instead of train case
-                kk = classification_report(outputs[data.idx_test].tolist(), lab[data.idx_test].tolist())
-                print(kk)
+                #TBD
--- models.py
+++ models.py
@@ -4,23 +4,16 @@ from layers import *
 import torch.nn.functional as F
 
 class GCN(nn.Module):
-    def __init__(self, nfeat, nhid, nclass, dropout):
+    def __init__(self, nfeat, nhid, dropout):
         super(GCN, self).__init__()
         self.gc1 = GraphConvolution(nfeat, nhid)
         self.gc2 = GraphConvolution(nhid, nhid)
         self.dropout = dropout
-        self.loss = nn.BCELoss()
         
-    def forward(self, x, adj, train_idx):
+    def forward(self, x, adj):
         x = F.relu(self.gc1(x, adj))
         x = F.dropout(x, self.dropout, training = self.training)
         x = self.gc2(x, adj)
-        adj_ = x @ x.T
-        out = self.loss(adj[train_idx], adj_[train_idx])
+        adj_pred = torch.sigmoid(torch.matmul(x, x.t()))
 
-        return out
-        
-        #x = F.dropout(x, self.dropout, training=self.training)
-        #x = self.gc2(x, adj)
-               
-        #return F.log_softmax(x, dim=1)
+        return adj_pred
